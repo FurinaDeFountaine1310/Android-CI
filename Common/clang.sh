2702e44b6366d164069a5a467db9e019e9cbfe1a
Furina de fountaine <furinadefountaine1310@gmail.com>
2025-07-19 19:37:13 +0700

    Update clang.sh
    Signed-off-by: Furina de fountaine <furinadefountaine1310@gmail.com>

--- Common/clang.sh
+++ Common/clang.sh
@@ -40,7 +40,7 @@ chmod +x build-llvm.py
     --vendor-string "$LLVM_NAME" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.
-[ ! -f install/bin/clang-2* ] && {
+[ ! -f install/bin/clang-1* ] && {
 	err "Building LLVM failed ! Kindly check errors !!"
 	err "build.log" "Error Log"
 	exit 1
