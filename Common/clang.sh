ec9a6f8b6ee94775cb70426a743112d727675f38
Furina de fountaine <furinadefountaine1310@gmail.com>
2025-07-20 05:16:54 +0700

    Update clang.sh
    Signed-off-by: Furina de fountaine <furinadefountaine1310@gmail.com>

--- Common/clang.sh
+++ Common/clang.sh
@@ -100,7 +100,7 @@ git lfs track "bugpoint"
 git lfs track "clang-scan-deps"
 git lfs track "lld"
 git lfs track "libclang.so.17.0.6"
-git lfs track "libclang-cpp.so.17.0"
+git lfs track "libclang-cpp.so.17"
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
 git commit -asm "Carlotta: Update to $rel_date build
